8c1fb2a218d69c5d9c355951a8c8cc843f704fd1
DanilaMakarov <dn.mkrv1321@gmail.com>
2023-07-28 16:16:45 +0300

    Update tests

--- sobes_project/employee_records/tests.py
+++ sobes_project/employee_records/tests.py
@@ -1,92 +1,81 @@
 import json
 
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.test import TestCase
-
+from rest_framework.reverse import reverse, reverse_lazy
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.test import APITestCase
 from employee_records.models import Employee
 
 
-# Create your tests here.
+class JWTAuthenticationTest(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user('Whoami')
+        cls.user.set_password('12345')
+        cls.user.save()
+        cls.token_url = reverse('token')
+
+    def test_authentication(self):
+        response = self.client.post(self.token_url, data={'username': 'Whoami',
+                                                          'password': '12345'},
+                                    )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'access')
+
+    def test_bad_credentials_authentication(self):
+        response = self.client.post(self.token_url, data={'username': 'Whoami',
+                                                          'password': '123456'})
+        self.assertEqual(response.status_code, 401)
 
 
-class AuthCRUDTestCase(TestCase):
+class AuthCRUDTestCase(APITestCase):
     fixtures = ['initial_data.json']
 
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user('Whoami')
-        cls.password = '12345'
-        cls.user.set_password(cls.password)
+        cls.user.set_password('12345')
         cls.user.save()
+        cls.token_url = reverse('token')
+        cls.headers = dict()
 
-    def test_list_employees(self):
-        token_response = self.client.post('/api/token/',
-                                          data={'username': self.user.username, 'password': self.password})
-        token = token_response.data['access']
-        headers = {'Authorization': f'Bearer {token}'}
-        response = self.client.get('/api/employee_records/', headers=headers)
-
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        response_data = self.client.post(self.token_url, data={'username': 'Whoami',
+                                                               'password': '12345'}).json()
+        self.headers['Authorization'] = f'Bearer {response_data["access"]}'
 
     def test_retrieve_employee(self):
-        token_response = self.client.post('/api/token/',
-                                          data={'username': self.user.username, 'password': self.password})
-        token = token_response.data['access']
-        headers = {'Authorization': f'Bearer {token}'}
-        response = self.client.get('/api/employee_records/1/', headers=headers)
+        response = self.client.get('/api/employee_records/1/', headers=self.headers)
 
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Project Manager')
 
     def test_create_employee(self):
-        token_response = self.client.post('/api/token/',
-                                          data={'username': self.user.username, 'password': self.password})
-        token = token_response.data['access']
-        headers = {'Authorization': f'Bearer {token}'}
-        data = {'name': 'who', 'surname': 'ami', 'job_title': 'Jun Dev', 'chiefs': [1, 2, 3, 4]}
-        response = self.client.post('/api/employee_records/', headers=headers, data=data)
-
+        data = {'name': 'Test',
+                'surname': 'Employee',
+                'job_title': 'Test Job Title',
+                'employees': [],
+                'chief': [1, ]}
+        response = self.client.post('/api/employee_records/',
+                                    headers=self.headers,
+                                    data=data)
         self.assertEqual(response.status_code, 201)
-
-    def test_update_employee(self):
-        token_response = self.client.post('/api/token/',
-                                          data={'username': self.user.username, 'password': self.password})
-        token = token_response.data['access']
-        headers = {'Authorization': f'Bearer {token}'}
-        data = {'name': 'who', 'surname': 'ami'}
-        response = self.client.patch('/api/employee_records/1/', content_type='application/json', data=data,
-                                     headers=headers)
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test', status_code=201)
 
     def test_delete_employee(self):
-        token_response = self.client.post('/api/token/',
-                                          data={'username': self.user.username, 'password': self.password})
-        token = token_response.data['access']
-        headers = {'Authorization': f'Bearer {token}'}
-        response = self.client.delete('/api/employee_records/1/', headers=headers)
-
+        employee_url = '/api/employee_records/1/'
+        response = self.client.delete(employee_url, headers=self.headers)
         self.assertEqual(response.status_code, 204)
 
-
-class NoAuthCRUDTestCase(TestCase):
-    def test_list_employees(self):
-        response = self.client.get('/api/employee_records/')
-        self.assertEqual(response.status_code, 401)
-
-    def test_retrieve_employee(self):
-        response = self.client.get('/api/employee_records/1/')
-        self.assertEqual(response.status_code, 401)
-
-    def test_create_employee(self):
-        data = {'name': 'who', 'surname': 'ami', 'job_title': 'Jun Dev', 'chiefs': [1, 2, 3, 4]}
-        response = self.client.post('/api/employee_records/', data=data)
-
-        self.assertEqual(response.status_code, 401)
-
     def test_update_employee(self):
-        data = {'name': 'who', 'surname': 'ami'}
-        response = self.client.patch('/api/employee_records/1/', content_type='application/json', data=data)
-        self.assertEqual(response.status_code, 401)
+        employee_url = '/api/employee_records/1/'
+        data = {'name': 'TestUpdate'}
+        response = self.client.patch(employee_url, data=data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'TestUpdate')
 
-    def test_delete_employee(self):
-        response = self.client.delete('/api/employee_records/1/')
-        self.assertEqual(response.status_code, 401)
+
